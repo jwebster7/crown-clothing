3c5c4e93179f7890ad260467c9a35eb30676c115
jwebster7 <>
2020-12-29 11:27:00 -0600

    created a basic header component and added a logo to it

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
 
+import Header from "./components/header/header.component";
 import HomePage from "./pages/home/home.component";
 import ShopPage from "./pages/shop/shop.component";
 
@@ -10,6 +11,7 @@ import ShopPage from "./pages/shop/shop.component";
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
