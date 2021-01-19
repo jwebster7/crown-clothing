aaa9cb6c80dac9aaa5b44e6d485deae30df2257f
jwebster7 <>
2021-01-19 12:59:19 -0600

    setup a persistant cart feature for users so that when they sign out and sign in the cart items persist in firebase.

--- client/src/redux/cart/cart.sagas.js
+++ client/src/redux/cart/cart.sagas.js
@@ -1,7 +1,13 @@
-import { all, call, takeLatest, put } from "redux-saga/effects";
+import { all, call, takeLatest, put, select } from "redux-saga/effects";
 
+import { getUserCartRef } from "../../firebase/firebase.utils";
+
+import { selectCurrentUser } from "../user/user.selector";
 import UserActionTypes from "../user/user.types";
-import { clearCart } from "./cart.actions";
+
+import { clearCart, setCartFromFirebase } from "./cart.actions";
+import { selectCartItems } from "./cart.selectors";
+import CartActionTypes from "../cart/cart.types";
 
 export function* clearCartOnSignOut() {
   yield put(clearCart());
@@ -11,6 +17,43 @@ export function* onSignOutSuccess() {
   yield takeLatest(UserActionTypes.SIGN_OUT_SUCCESS, clearCartOnSignOut);
 }
 
+export function* onUserSignIn() {
+  yield takeLatest(UserActionTypes.SIGN_IN_SUCCESS, setUserCartFromFirebase);
+}
+
+export function* updateUserCartInFirebase() {
+  const user = yield select(selectCurrentUser);
+
+  if (user) {
+    try {
+      const cartRef = yield getUserCartRef(user.id);
+      const cartItemsToAdd = yield select(selectCartItems);
+      yield cartRef.update({ cartItems: cartItemsToAdd });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+
+export function* setUserCartFromFirebase({ payload: user }) {
+  const cartRef = yield getUserCartRef(user.id);
+  const cartSnapShot = yield cartRef.get();
+  const cartItems = cartSnapShot.data().cartItems;
+  yield put(setCartFromFirebase(cartItems));
+}
+
+export function* onCartChange() {
+  yield takeLatest(
+    [
+      CartActionTypes.ADD_CART_ITEM,
+      CartActionTypes.REMOVE_CART_ITEM,
+      CartActionTypes.CLEAR_CART_ITEM
+    ],
+    updateUserCartInFirebase
+  );
+}
+
 export function* cartSagas() {
-  yield all([call(onSignOutSuccess)]);
+  // yield all([call(onSignOutSuccess), call(onUserSignIn), call(onCartChange)]);
+  yield all([call(onSignOutSuccess), call(onCartChange), call(onUserSignIn)]);
 }
