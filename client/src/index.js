6a97344f010768a1e42bc7ee797baaf33bfaa857
jwebster7 <>
2021-01-18 22:10:31 -0600

    added files, updated index.html, and updated some dependencies for PWA.

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,12 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import { PersistGate } from "redux-persist/integration/react";
-// import reportWebVitals from './reportWebVitals';
 
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
 
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <Provider store={store}>
@@ -22,9 +22,12 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-serviceWorker.register();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
